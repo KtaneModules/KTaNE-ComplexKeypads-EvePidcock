c7fec5438c15c6add0ca404f9974dc4b22a31546
AAces <djp0908@yahoo.com>
2018-05-31 12:34:48 -0600

    Initial Commit

---
+++ Assets/complexKeypads.cs
@@ -0,0 +1,354 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using Newtonsoft.Json;
+using KMHelper;
+using System;
+using System.Threading;
+using UnityEngine;
+
+public class complexKeypads : MonoBehaviour
+{
+
+    public KMAudio newAudio;
+    public KMBombModule module;
+    public KMBombInfo info;
+    public KMModSettings modSettings;
+    public KMSelectable[] btn;
+    public MeshRenderer[] leds;
+    public string[] symbols;
+
+    public Material off;
+    public Material green;
+    public Material red;
+
+    private static int _moduleIdCounter = 1;
+    private int _moduleId = 0;
+
+    private bool _isSolved = false, _lightsOn = false;
+    public int symbolRow = 1;
+    private int[] one = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    private int[] two = new int[10] { 10, 0, 6, 11, 12, 13, 14, 15, 16, 17 };
+    private int[] three = new int[10] { 18, 17, 11, 13, 2, 8, 1, 10, 19, 9 };
+    private int[] four = new int[10] { 19, 15, 18, 1, 4, 11, 0, 7, 17, 20 };
+    private int[] five = new int[10] { 13, 11, 4, 12, 20, 16, 5, 0, 14, 8 };
+    public int[] buttonNumbers = new int[9];
+    public string[] buttonStrings = new string[9];
+    public int[] list = new int[9];
+    public int[] buttonPressOrder = new int[9];
+    public int[] symbolSet;
+
+    public int pressIndex = 0;
+
+    public int ruleInUse = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+        module.OnActivate += Activate;
+    }
+
+    private void Awake()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            int j = i;
+            btn[i].OnInteract += delegate ()
+            {
+                handlePress(j);
+                return false;
+            };
+        }       
+    }
+
+    // Update is called once per frame
+    void Activate()
+    {
+        Init();
+        _lightsOn = true;
+    }
+
+    void Init()
+    {
+        symbolRow = UnityEngine.Random.Range(1, 6);
+        setupSymbols();
+        setupButtons();
+        setupPressOrder();
+        setupLEDS();
+    }
+
+    void setupLEDS()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            leds[i].material = off;
+        }
+    }
+
+    void setupSymbols()
+    {
+        symbols[0] = "α";
+        symbols[1] = "ε";
+        symbols[2] = "θ";
+        symbols[3] = "ψ";
+        symbols[4] = "μ";
+        symbols[5] = "Ξ";
+        symbols[6] = "ζ";
+        symbols[7] = "σ";
+        symbols[8] = "β";
+        symbols[9] = "Δ";
+        symbols[10] = "π";
+        symbols[11] = "ω";
+        symbols[12] = "δ";
+        symbols[13] = "Γ";
+        symbols[14] = "η";
+        symbols[15] = "م";
+        symbols[16] = "⊃";
+        symbols[17] = "κ";
+        symbols[18] = "φ";
+        symbols[19] = "נ";
+        symbols[20] = "ن";
+    }
+    void setupButtons()
+    {
+        buttonNumbers[0] = UnityEngine.Random.Range(0, 10);
+        buttonNumbers[1] = UnityEngine.Random.Range(0, 10);
+        do
+        {
+            buttonNumbers[1] = UnityEngine.Random.Range(0, 10);
+        } while (buttonNumbers[1] == buttonNumbers[0]);
+        buttonNumbers[2] = UnityEngine.Random.Range(0, 10);
+        do
+        {
+            buttonNumbers[2] = UnityEngine.Random.Range(0, 10);
+        } while (buttonNumbers[2] == buttonNumbers[0] || buttonNumbers[2] == buttonNumbers[1]);
+        buttonNumbers[3] = UnityEngine.Random.Range(0, 10);
+        do
+        {
+            buttonNumbers[3] = UnityEngine.Random.Range(0, 10);
+        } while (buttonNumbers[3] == buttonNumbers[0] || buttonNumbers[3] == buttonNumbers[1] || buttonNumbers[3] == buttonNumbers[2]);
+        buttonNumbers[4] = UnityEngine.Random.Range(0, 10);
+        do
+        {
+            buttonNumbers[4] = UnityEngine.Random.Range(0, 10);
+        } while (buttonNumbers[4] == buttonNumbers[0] || buttonNumbers[4] == buttonNumbers[1] || buttonNumbers[4] == buttonNumbers[2] || buttonNumbers[4] == buttonNumbers[3]);
+        buttonNumbers[5] = UnityEngine.Random.Range(0, 10);
+        do
+        {
+            buttonNumbers[5] = UnityEngine.Random.Range(0, 10);
+        } while (buttonNumbers[5] == buttonNumbers[0] || buttonNumbers[5] == buttonNumbers[1] || buttonNumbers[5] == buttonNumbers[2] || buttonNumbers[5] == buttonNumbers[3] || buttonNumbers[5] == buttonNumbers[4]);
+        buttonNumbers[6] = UnityEngine.Random.Range(0, 10);
+        do
+        {
+            buttonNumbers[6] = UnityEngine.Random.Range(0, 10);
+        } while (buttonNumbers[6] == buttonNumbers[0] || buttonNumbers[6] == buttonNumbers[1] || buttonNumbers[6] == buttonNumbers[2] || buttonNumbers[6] == buttonNumbers[3] || buttonNumbers[6] == buttonNumbers[4] || buttonNumbers[6] == buttonNumbers[5]);
+        buttonNumbers[7] = UnityEngine.Random.Range(0, 10);
+        do
+        {
+            buttonNumbers[7] = UnityEngine.Random.Range(0, 10);
+        } while (buttonNumbers[7] == buttonNumbers[0] || buttonNumbers[7] == buttonNumbers[1] || buttonNumbers[7] == buttonNumbers[2] || buttonNumbers[7] == buttonNumbers[3] || buttonNumbers[7] == buttonNumbers[4] || buttonNumbers[7] == buttonNumbers[5] || buttonNumbers[7] == buttonNumbers[6]);
+        buttonNumbers[8] = UnityEngine.Random.Range(0, 10);
+        do
+        {
+            buttonNumbers[8] = UnityEngine.Random.Range(0, 10);
+        } while (buttonNumbers[8] == buttonNumbers[0] || buttonNumbers[8] == buttonNumbers[1] || buttonNumbers[8] == buttonNumbers[2] || buttonNumbers[8] == buttonNumbers[3] || buttonNumbers[8] == buttonNumbers[4] || buttonNumbers[8] == buttonNumbers[5] || buttonNumbers[8] == buttonNumbers[6] || buttonNumbers[8] == buttonNumbers[7]);
+
+
+        switch (symbolRow)
+        {
+            case 1:
+                symbolSet = one;
+                break;
+            case 2:
+                symbolSet = two;
+                break;
+            case 3:
+                symbolSet = three;
+                break;
+            case 4:
+                symbolSet = four;
+                break;
+            case 5:
+                symbolSet = five;
+                break;
+            default:
+                symbolSet = one;
+                Debug.LogFormat("[Complex Keypads #{0}] Symbol row not selected correctly! Random row returned {1}", _moduleId, symbolRow);
+                break;
+        }
+
+
+
+        for (int i = 0; i < 9; i++)
+        {
+            list[i] = symbolSet[buttonNumbers[i]];
+        }
+
+        for (int i = 0; i < 9; i++)
+        {
+            buttonStrings[i] = symbols[list[i]];
+        }
+
+        if (info.GetBatteryCount() > 2 && info.IsPortPresent(KMBombInfoExtensions.KnownPortType.Parallel))
+        {
+            ruleInUse = 1;
+        }
+        else if (info.IsPortPresent(KMBombInfoExtensions.KnownPortType.DVI) && info.IsIndicatorOn(KMBombInfoExtensions.KnownIndicatorLabel.BOB))
+        {
+            ruleInUse = 2;
+        }
+        else
+        {
+            ruleInUse = 3;
+        }
+
+        for (int i = 0; i < 9; i++)
+        {
+            btn[i].GetComponentInChildren<TextMesh>().text = buttonStrings[i];
+        }
+
+    }
+    void setupPressOrder()
+    {
+        switch (ruleInUse)
+        {
+            case 1:
+                for (int i = 0; i < 9; i++)
+                {
+                    buttonPressOrder[i] = i;
+                }
+                break;
+            case 2:
+                int[] reverseSet = symbolSet;
+                for (int i = 0; i < reverseSet.Length / 2; i++)
+                {
+                    int tmp = reverseSet[i];
+                    reverseSet[i] = reverseSet[reverseSet.Length - i - 1];
+                    reverseSet[reverseSet.Length - i - 1] = tmp;
+                }
+                int removal = 0;
+                for (int i = 0; i < 9; i++)
+                {
+                    if (!list.Contains(reverseSet[i]))
+                    {
+                        removal = i;
+                        break;
+                    }
+                }
+                int[] btnSymbolSet = new int[9];
+                bool t = false;
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i == removal)
+                    {
+                        t = true;
+                        continue;
+                    }
+                    if (!t)
+                    {
+                        btnSymbolSet[i] = reverseSet[i];
+                    }
+                    else
+                    {
+                        btnSymbolSet[i - 1] = reverseSet[i];
+                    }
+                }
+                foreach (int i in btnSymbolSet)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (list[j] == i)
+                        {
+                            buttonPressOrder[Array.IndexOf(btnSymbolSet, i)] = j;
+                            break;
+                        }
+                    }
+                }
+                break;
+            case 3:
+                int removalX = 0;
+                for (int i = 0; i < 9; i++)
+                {
+                    if (!list.Contains(symbolSet[i]))
+                    {
+                        removalX = i;
+                        break;
+                    }
+                }
+                int[] btnSymbolSetX = new int[9];
+                bool tX = false;
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i == removalX)
+                    {
+                        tX = true;
+                        continue;
+                    }
+                    if (!tX)
+                    {
+                        btnSymbolSetX[i] = symbolSet[i];
+                    }
+                    else
+                    {
+                        btnSymbolSetX[i - 1] = symbolSet[i];
+                    }
+                }
+                foreach (int i in btnSymbolSetX)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (list[j] == i)
+                        {
+                            buttonPressOrder[Array.IndexOf(btnSymbolSetX, i)] = j;
+                            break;
+                        }
+                    }
+                }
+                break;
+        }
+    }
+    void resetGame()
+    {
+        setupButtons();
+        setupPressOrder();
+        setupLEDS();
+    }
+    void handlePress(int btnIndex)
+    {
+        newAudio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, btn[btnIndex].transform);
+        if (!_lightsOn || _isSolved) return;
+
+        if(buttonPressOrder[pressIndex] == btnIndex)
+        {
+            pressIndex++;
+            leds[btnIndex].material = green;
+        } else
+        {
+            module.HandleStrike();
+            pressIndex = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                leds[i].material = red;
+            }
+            StartCoroutine(RedLights());
+            return;
+        }
+        if(pressIndex == 9)
+        {
+            module.HandlePass();
+            _isSolved = true;
+            newAudio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.CorrectChime, btn[btnIndex].transform);
+        }
+    }
+    IEnumerator RedLights()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            leds[i].material = red;
+        }
+        yield return new WaitForSeconds(2.0f);
+        resetGame();
+    }
+}
